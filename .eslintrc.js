4160c615e46854e14a6f480ccdc12b2fc8cb68e9
Adam Butterworth <abutterworth@edx.org>
2020-03-24 17:55:36 -0400

    fix: allow labels to satisfy either a11y requirement instead of both

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,13 @@ module.exports = {
       }
     ],
     'arrow-parens': 'off',
+    'jsx-a11y/label-has-associated-control': ['error', {
+      labelComponents: [],
+      labelAttributes: [],
+      controlComponents: [],
+      assert: 'either',
+      depth: 25
+    }],
     'react/jsx-props-no-spreading': 'off',
     'react/jsx-one-expression-per-line': 'off',
     'react/destructuring-assignment': 'off',
